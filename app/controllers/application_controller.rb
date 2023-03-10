c15c30881545a65de22212f8ce578204bc7f86e8
Sienna <remicknsisi@gmail.com>
2023-03-10 18:43:40 -0500

    commit tree f4134308003a2d1c18334d742ba3857a7c75a949

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,20 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(params)
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(params)
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find_by(id: params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
